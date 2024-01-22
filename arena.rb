73476a45badb6eac9315456499f23d18fb09c36c
janpatrickpalacio <janpatrick.dev@gmail.com>
2024-01-22 14:30:07 +0800

    completed activity

--- arena.rb
+++ arena.rb
@@ -1,10 +1,14 @@
 class Arena
+  ACTIONS = ["attack", "use_potion"].freeze
   # 3. add a reader for both player and enemy
   # enter code here
+  attr_reader :player, :enemy
 
   def initialize(player:, enemy:)
     # 2. Create an instance variable for player and enemy
     # enter code here
+    @player = player
+    @enemy = enemy
   end
 
   def start
@@ -14,6 +18,12 @@ class Arena
       # 4. Implement actions where Player and Enemey will both fight. Player and Enemy will do a random action.
       # ACTIONS are: attack and use_potion
       # enter code here
+
+      # player action
+      make_action(ACTIONS.sample, player, enemy)
+
+      # enemy action
+      make_action(ACTIONS.sample, enemy, player)
 
       puts "Your health: #{player.hp}"
       puts "#{enemy.name}'s health: #{enemy.hp}"
@@ -27,6 +37,14 @@ class Arena
   end
 
   private
+
+    def make_action(action, attacker, target)
+      if action == "use_potion" && attacker.potions > 0
+        attacker.use_potion
+      else
+        self.attack(attacker, target)
+      end
+    end
 
     def attack(attacker, target)
       damage = attacker.atk - target.def
--- player.rb
+++ player.rb
@@ -1,7 +1,7 @@
 class Player
   MAX_POTIONS_QTY = 5.freeze
 
-  attr_accessor :name, :potions, :hp
+  attr_accessor :name, :potions, :hp, :action
   attr_reader :atk, :def
 
   def initialize(name)
@@ -10,6 +10,7 @@ class Player
     @atk = 20
     @def = 5
     @potions = MAX_POTIONS_QTY
+    @action = ""
   end
 
   # 1. Create a method where player can heal using potions and reduce the player's potion amount
@@ -17,7 +18,9 @@ class Player
   def use_potion
     # Prevent using of potions when no more potions left. Hint: use guard clause here
     # enter code here
-
+    return false if self.potions == 0
+    self.hp += 15
+    self.potions -= 1
     puts "#{name} used potions."
   end
 end
